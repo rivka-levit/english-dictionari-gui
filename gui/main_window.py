69a35050c09e667a0bc3075d17e8a4bcc1c3b707
rivka-yuliia <juliacreadora@gmail.com>
2023-11-15 21:33:27 +0200

    button and input style

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QFont
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -35,6 +35,17 @@ class MainWindow(QMainWindow):
         # widget.setStyleSheet('background-color: #F2EDDC;')
         widget.setContentsMargins(20, 20, 20, 20)
         self.setCentralWidget(widget)
+        self.setStyleSheet(
+            """
+                QWidget {
+                    background-color: #f5efe6;
+                }
+                QLabel {
+                    border: 2px solid #fefcf9;
+                    border-radius: 5px;
+                }
+            """
+        )
 
     def _set_main_layout(self):
         main_layout = QVBoxLayout()
@@ -44,15 +55,49 @@ class MainWindow(QMainWindow):
         main_layout.addLayout(self._set_nested_layout())
 
         self.definitions.setMinimumHeight(100)
+        self.definitions.setFont(QFont('Helvetica', 12))
         main_layout.addWidget(self.definitions)
 
         return main_layout
 
     def _set_nested_layout(self):
         nest_layout = QHBoxLayout()
+
+        self.wrd_input.setFont(QFont('Helvetica', 14))
+        self.wrd_input.setPlaceholderText('Enter a word here...')
+        self.wrd_input.setStyleSheet(
+            """
+                QLineEdit {
+                    padding: 6px;
+                    border: 2px solid #aebdca;
+                    border-radius: 5px;
+                    font-size: 26;
+                }
+            """
+        )
         nest_layout.addWidget(self.wrd_input, stretch=5)
 
-        btn = QPushButton('Convert')
+        btn = QPushButton('Send')
+        btn.setFixedHeight(40)
+        btn.setFont(QFont('Helvetica', 16))
+        btn.setStyleSheet(
+            """
+                QPushButton {
+                    padding: 6px;
+                    background-color: #7895b2;
+                    color: #f5efe6;
+                    border-radius: 5px;
+                }
+                QPushButton:hover {
+                    background-color: #849fb9;
+                    color: #f5efe6;
+                }
+                QPushButton:pressed {
+                    background-color: #486683;
+                    color: #f5efe6;
+                }
+            """
+        )
         nest_layout.addWidget(btn, stretch=1)
         btn.clicked.connect(self._set_output)
 
@@ -61,8 +106,11 @@ class MainWindow(QMainWindow):
     def _set_output(self):
         dct = ApiDictionary()
         word = self.wrd_input.text()
-        self.definitions.setText('\n'.join(dct.get_definition(word)))
-        self.wrd_input.clear()
+        if word:
+            self.definitions.setText('\n'.join(dct.get_definition(word)))
+            self.wrd_input.clear()
+        else:
+            self.definitions.setText('')
 
     def _center(self):
         qt_rectangle = self.frameGeometry()
