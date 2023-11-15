aba76f96df0d0a1c590e41f2bfc314388f282e3f
rivka-yuliia <juliacreadora@gmail.com>
2023-11-15 17:58:12 +0200

    split to methods

--- gui/main_window.py
+++ gui/main_window.py
@@ -17,35 +17,43 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.wrd_input = QLineEdit()
+        self.definitions = QLabel('')
+        self._set_main_window()
+
+    def _set_main_window(self):
         self.setWindowTitle('Word Definition')
         self.setGeometry(0, 0, 500, 300)
+        self._center()
 
-        self.center()
+        widget = QWidget()
+        widget.setLayout(self._set_main_layout())
+        # widget.setStyleSheet('background-color: #F2EDDC;')
+        widget.setContentsMargins(20, 20, 20, 20)
+        self.setCentralWidget(widget)
 
+    def _set_main_layout(self):
         main_layout = QVBoxLayout()
         main_layout.setSpacing(15)
 
+        main_layout.addLayout(self._set_nested_layout())
+
+        self.definitions.setMinimumHeight(100)
+        main_layout.addWidget(self.definitions)
+
+        return main_layout
+
+    def _set_nested_layout(self):
         nest_layout = QHBoxLayout()
 
-        self.wrd_input = QLineEdit()
         nest_layout.addWidget(self.wrd_input, stretch=5)
 
         btn = QPushButton('Convert')
         nest_layout.addWidget(btn, stretch=1)
 
-        self.definitions = QLabel('')
-        self.definitions.setMinimumHeight(100)
+        return nest_layout
 
-        main_layout.addLayout(nest_layout)
-        main_layout.addWidget(self.definitions)
-
-        widget = QWidget()
-        widget.setLayout(main_layout)
-        # widget.setStyleSheet('background-color: #F2EDDC;')
-        widget.setContentsMargins(20, 20, 20, 20)
-        self.setCentralWidget(widget)
-
-    def center(self):
+    def _center(self):
         qt_rectangle = self.frameGeometry()
         center_point = self.screen().availableGeometry().center()
         qt_rectangle.moveCenter(center_point)
